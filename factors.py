346a46d7404f7a553f1c9f2bd098242e481d4c25
Anthony <rappan@oregonstate.edu>
2024-01-24 17:05:12 -0800

    edited range

--- factors.py
+++ factors.py
@@ -5,8 +5,8 @@
 
 pos_int = int(input("Please enter a positive integer: "))
 print("The factors of ", pos_int, " are: ")
-print(1)
-for value in range(2, pos_int):
+
+for value in range(1, pos_int):
     if pos_int % value == 0:
         print(value)
 print(pos_int)
