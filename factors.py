dae77d232295155d1b83a1773121b35225daf965
Anthony <rappan@oregonstate.edu>
2024-01-24 17:16:18 -0800

    edited range

--- factors.py
+++ factors.py
@@ -4,7 +4,7 @@
 #Assignment 3b, print facotrs of chosen positive integer
 
 pos_int = int(input("Please enter a positive integer:"))
-print("The factors of", pos_int, "are:")
+print("The factors of12", pos_int, "are :")
 
 for value in range(1, pos_int):
     if pos_int % value == 0:
